46ea124b35805ee00a00af0b24ccde3243810a3f
ＲＹＡＮ＿ＣＨＡＮＧ <ccae3214@gmail.com>
2019-07-22 14:16:39 +0800

    072221416

--- server/app.js
+++ server/app.js
@@ -1,15 +1,19 @@
 const express = require('express')
+var proxy = require('http-proxy-middleware');
+
 const path = require('path')
 const logger = require('morgan')
+const cors = require('cors');
 const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
-
+const helmet = require('helmet');
 const imagesUpload = require('images-upload-middleware')
 
 const index = require('./routes/index')
 
 const app = express()
-
+app.use(helmet());
+app.use(cors());
 // uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'))
@@ -26,7 +30,28 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
 app.use('/api', index)
-app.get('*', (req, res) => {
+var options = {
+  target: 'https://matchtrend.herokuapp.com/', // target host
+  changeOrigin: true, // needed for virtual hosted sites
+  ws: true, // proxy websockets
+  pathRewrite: {
+    '^/api/old-path': '/api/new-path', // rewrite path
+    '^/api/remove/path': '/path' // remove base path
+  },
+  router: {
+    // when request.headers.host == 'dev.localhost:3000',
+    // override target 'http://www.example.org' to 'http://localhost:8000'
+    'dev.localhost:3000': 'http://localhost:5000'
+  }
+};
+
+// create the proxy (without context)
+var exampleProxy = proxy(options);
+
+// mount `exampleProxy` in web server
+app.use('/api/*', exampleProxy);
+
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../build', 'index.html'))
 })
 
--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
-const baseAPI = '/api';
+const baseAPI = '/api'
 
 const Service = {
-  
+
   create_student(create_student) {
     return new Promise((resolve, reject) => {
       fetch(`${baseAPI}/create_student`, {
@@ -21,14 +21,14 @@ const Service = {
   },
   create_family_background(create_family_background) {
     return new Promise((resolve, reject) => {
-      fetch(`${baseAPI}/create_family_background`,{
+      fetch(`${baseAPI}/create_family_background`, {
         method: 'POST',
         body: JSON.stringify(create_family_background),
         headers: {
           Accept: 'application/json',
           'Content-Type': 'application/json'
         }
-      }) .then(response => response.json())
+      }).then(response => response.json())
         .then(result => {
           resolve(result);
         })
